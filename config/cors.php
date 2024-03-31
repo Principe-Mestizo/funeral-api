2faebd3d8c9193d2d2703cb8cf6bc98e4d37896d
Principe Mestizo <vquispealarcon@gmail.com>
2024-03-30 19:55:59 -0500

    funeraria backend cors

--- config/cors.php
+++ config/cors.php
@@ -17,18 +17,16 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://principe-mestizo.github.io/senati-funeraria/'],
 
-    'allowed_origins_patterns' => [],
-
-    'allowed_headers' => ['*'],
+    'allowed_headers' => ['Content-Type', 'Authorization'],
 
     'exposed_headers' => [],
 
-    'max_age' => 0,
+    'max_age' => 3600,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
